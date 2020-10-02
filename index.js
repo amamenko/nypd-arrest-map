8a60edc5f3114bd1022fdc3f14cebfe89b332d64
Avraham Mamenko <amamenko@hotmail.com>
2020-10-02 00:08:56 -0400

    Changed nodejs port config

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const storage = new Storage({
   credentials: require("./nypd-arrest-map-details.js"),
