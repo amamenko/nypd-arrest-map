9498977f880e7b9e96a960153f33378e2d75a212
Avraham Mamenko <amamenko@hotmail.com>
2020-11-23 15:27:41 -0500

    Changed production path to index.html file in server

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("Client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("index.html"));
+    res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
   });
 }
 
